17be2c7621d7eac7580aeac610a8477e062a71d0
Samantha Chikowore <tanakiswa@icloud.com>
2025-12-06 21:44:38 +0400

    created CRUD

--- CST1510CW2/migrate_users.py
+++ CST1510CW2/migrate_users.py
@@ -8,4 +8,24 @@ def migrate_users(json_file="users.json"):
     try:
         with open(json_file, "r") as f:
             users = json.load(f)
-        except
+    except FileNotFoundError:
+        print("users.json not found.")
+        return
+    
+    for username, data in users.items():
+        db.execute_query("""
+            INSERT OR IGNORE INTO users (username, password_hash, role, created_at, last_login)
+            VALUES (?, ?, ?, ?, ?)
+        """, (
+            username,
+            data["password_hash"],
+            data["role"],
+            data["created_at"],
+            data["last_login"]
+        ))
+
+    print("Migration complete!")
+    db.disconnect()
+
+if __name__ == "main":
+    migrate_users()
