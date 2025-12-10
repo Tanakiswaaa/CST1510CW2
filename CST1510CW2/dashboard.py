b76e5ee241b93cd7c62301dbd390762373d5e441
Samantha Chikowore <tanakiswa@icloud.com>
2025-12-11 02:56:59 +0400

    added syste health for monitoring

---
+++ CST1510CW2/dashboard.py
@@ -0,0 +1,8 @@
+if st.button("Cybersecurity", use_container_width=True):
+    st.switch_page("pages/cybersecurity_dashboard.py")
+
+if st.button("Data Science", use_container_width=True):
+    st.switch_page("pages/data_science_dashboard.py")
+
+if st.button("IT Operations", use_container_width=True):
+    st.switch_page("pages/it_operations_dashboard.py")
