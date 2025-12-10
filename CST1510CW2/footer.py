f220ad5aaf4d91456ca3085bbca04809079de630
Samantha Chikowore <tanakiswa@icloud.com>
2025-12-11 03:06:18 +0400

    added footer to pages

---
+++ CST1510CW2/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from datetime import datetime
+
+def render_footer():
+    st.markdown("---")
+    st.caption(f"""
+    Multi-Domain Intelligence Platform  
+    Version 1.0 – Final Release  
+    © {datetime.now().year}
+    """)
